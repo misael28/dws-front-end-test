0a807471e0409fcd8bf7bcf56cfdb01aad34d754
Misael <misael.geof@gmail.com>
2025-08-24 11:04:09 -0300

    feat: add api integration and configuration

---
+++ src/services/api.ts
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type { Post, Author, Category } from '@/types'
+
+export const api = createApi({
+  reducerPath: 'dwsApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://tech-test-backend.dwsbrazil.io/' }),
+  tagTypes: ['Posts','Post','Authors','Categories'],
+  endpoints: (builder) => ({
+    getPosts: builder.query<Post[], void>({
+      query: () => 'posts/',
+      providesTags: (result) => result ? [
+        ...result.map(p => ({ type: 'Post' as const, id: p.id })),
+        { type: 'Posts', id: 'LIST' }
+      ] : [{ type: 'Posts', id: 'LIST' }]
+    }),
+    getPost: builder.query<Post, number>({
+      query: (id) => `posts/${id}`,
+      providesTags: (_res, _err, id) => [{ type: 'Post', id }]
+    }),
+    getAuthors: builder.query<Author[], void>({
+      query: () => 'authors/',
+      providesTags: [{ type: 'Authors', id: 'LIST' }]
+    }),
+    getAuthor: builder.query<Author, number>({
+      query: (id) => `authors/${id}`
+    }),
+    getCategories: builder.query<Category[], void>({
+      query: () => 'categories/',
+      providesTags: [{ type: 'Categories', id: 'LIST' }]
+    }),
+    getCategory: builder.query<Category, number>({
+      query: (id) => `categories/${id}`
+    }),
+  })
+})
+
+export const {
+  useGetPostsQuery,
+  useGetPostQuery,
+  useGetAuthorsQuery,
+  useGetAuthorQuery,
+  useGetCategoriesQuery,
+  useGetCategoryQuery
+} = api
